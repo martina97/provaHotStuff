da7bebc6e0efafa8c1a38f1c40d9fc2fc4cf5700
martina97 <martinadem97@gmail.com>
2024-01-14 17:34:36 +0100

    problemi dipendenze

---
+++ examples/hotstuff_app.cpp
@@ -0,0 +1,14 @@
+//
+// Created by martina on 14/01/24.
+//
+
+#include <iostream>
+#include "salticidae/stream.h"
+#include "salticidae/util.h"
+#include "salticidae/network.h"
+#include "salticidae/msg.h"
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+
+}
